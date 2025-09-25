5d4f26b5ce5510979722e4850c188ec95dc8c77c
hvsy <sumenghua@live.com>
2025-09-25 16:45:50 +0800

    修复tiktok浏览器端购买没有发送事件id的问题

--- src/shopify/additional/fragments/Product.tsx
+++ src/shopify/additional/fragments/Product.tsx
@@ -1,5 +1,5 @@
 import {FC, useEffect, useMemo, useRef, useState} from "react";
-import {get,find} from "lodash-es";
+import {get, find} from "lodash-es";
 import {VariantPrice, Variants} from "./Variants.tsx";
 import {Covers} from "./Covers.tsx";
 import {InputNumber} from "./InputNumber.tsx";
@@ -9,39 +9,39 @@ import {api} from "@lib/api.ts";
 import {PromiseLocation} from "../../lib/payment.ts";
 
 export type ProductProps = {
-    product : any;
-    shop : any;
+    product: any;
+    shop: any;
 };
 
 export const Product: FC<ProductProps> = (props) => {
-    const {product,shop} = props;
-    const  variants =get(product,'variants.edges',[]).map((edge : any) => {
+    const {product, shop} = props;
+    const variants = get(product, 'variants.edges', []).map((edge: any) => {
         const node = edge.node;
-        const options = node.selectedOptions.map((o : any) => {
+        const options = node.selectedOptions.map((o: any) => {
             return o.value;
         });
         return {
-            id : node.id,
-            title : node.title,
+            id: node.id,
+            title: node.title,
             options,
-            option1 : options?.[0],
-            option2 : options?.[1],
-            option3 : options?.[2],
-            price :  node.price,
-            availableForSale : node.availableForSale,
+            option1: options?.[0],
+            option2: options?.[1],
+            option3: options?.[2],
+            price: node.price,
+            availableForSale: node.availableForSale,
         }
     });
     const config = useMemo(() => {
         return {
-            images : get(product,'media.edges',[]).map((edge: any) => {
+            images: get(product, 'media.edges', []).map((edge: any) => {
                 return edge.node;
             }),
         }
-    },[product]);
-    const options = get(product,'options',[]).map((opt : any) => {
+    }, [product]);
+    const options = get(product, 'options', []).map((opt: any) => {
         return {
-            name : opt.name,
-            values : opt.optionValues.map((ov : any) => {
+            name: opt.name,
+            values: opt.optionValues.map((ov: any) => {
                 return ov.name;
             })
         }
@@ -53,24 +53,25 @@ export const Product: FC<ProductProps> = (props) => {
     const [quantity, setQuantity] = useState(1)
 
     const variant = useMemo(() => {
-        return find(variants, (v : any) => {
-            const opts = v.options|| [];
-            for(let i = 0; i < opts.length; ++i){
-                if(opts[i] !== selectedOptions[i]){
+        return find(variants, (v: any) => {
+            const opts = v.options || [];
+            for (let i = 0; i < opts.length; ++i) {
+                if (opts[i] !== selectedOptions[i]) {
                     return false;
                 }
             }
             return true;
         });
-    }, [variants,...selectedOptions]);
+    }, [variants, ...selectedOptions]);
 
     const {token} = useParams();
     const formRef = useRef<HTMLFormElement>(null);
     return <div className={'flex flex-col items-stretch sm:flex-row sm:py-5 space-y-3 sm:space-y-0'}>
         <div className={'flex sm:flex-1 overflow-hidden'}>
-            <Covers images={config.images} selected={selectedOptions} />
+            <Covers images={config.images} selected={selectedOptions}/>
         </div>
-        <form ref={formRef} method={'post'} action={`/a/s/${token}/upsell`} className={'px-5 flex flex-col items-stretch sm:items-center sm:flex-1 overflow-hidden text-center space-y-5 sm:space-y-8'}>
+        <form ref={formRef} method={'post'} action={`/a/s/${token}/upsell`}
+              className={'px-5 flex flex-col items-stretch sm:items-center sm:flex-1 overflow-hidden text-center space-y-5 sm:space-y-8'}>
             <div className={'text-2xl font-bold min-w-4/5'}>
                 {product.title}
             </div>
@@ -84,46 +85,48 @@ export const Product: FC<ProductProps> = (props) => {
             >
                 {variant && <VariantPrice variant={variant} quantity={quantity}/>}
             </Variants>
-            <input type={'hidden'} name={'variant_id'} value={variant?.id} />
-            <input type={'hidden'} name={'product_id'} value={product.id} />
+            <input type={'hidden'} name={'variant_id'} value={variant?.id}/>
+            <input type={'hidden'} name={'product_id'} value={product.id}/>
             <div className={'flex flex-row items-center space-x-2 min-w-4/5'}>
                 <span>
                     Quantity:
                 </span>
                 <InputNumber name={'quantity'} value={quantity} min={1} onChange={(v) => {
-                   setQuantity(v);
+                    setQuantity(v);
                 }}/>
             </div>
             <div className={'h-[30px] sm:hidden'}></div>
             <div className={'fixed sm:static bottom-0 inset-x-0 flex flex-col items-stretch'}>
-                    <a href={""}
-                       className={'text-[#9ca3af] opacity-30 bg-transparent text-[10px] text-left'} style={{
-                        transform: 'matrix(0.75,0,0,0.75,0,0)',
-                        transformOrigin : 'top left',
-                        marginTop: 20
-                    }}>
-                        Continue without this
-                    </a>
-                    <AsyncButton onClick={async () => {
-                        if (!formRef.current) return;
-                        const url = formRef.current.action;
-                        const formData = new FormData(formRef.current);
-                        // console.log('url:',url,Object.fromEntries(formData));
-                        // return new Promise((resolve) => {
-                        //   setTimeout(resolve,30000);
-                        // });
-                        const res = await api({
-                            method: "post",
-                            'url': url,
-                            data: Object.fromEntries(formData),
-                        });
-                        if (res.redirect) {
-                            return PromiseLocation(res.redirect);
-                        }
-                    }}
-                                 className={'uppercase bg-yellow-500 hover:bg-yellow-600 py-4 text-lg px-12 rounded-none text-black h-auto max-w-full min-w-4/5'}>
+                <a href={""}
+                   className={'text-[#9ca3af] opacity-30 bg-transparent text-[10px] text-left'} style={{
+                    transform: 'matrix(0.75,0,0,0.75,0,0)',
+                    transformOrigin: 'top left',
+                    marginTop: 20
+                }}>
+                    Continue without this
+                </a>
+                <AsyncButton onClick={async () => {
+                    if (!formRef.current) return;
+                    const url = formRef.current.action;
+                    const formData = new FormData(formRef.current);
+                    // console.log('url:',url,Object.fromEntries(formData));
+                    // return new Promise((resolve) => {
+                    //   setTimeout(resolve,30000);
+                    // });
+                    const res = await api({
+                        method: "post",
+                        'url': url,
+                        data: Object.fromEntries(formData),
+                    });
+                    if (res.redirect) {
+                        return PromiseLocation(res.redirect);
+                    }
+                }}
+                             className={'uppercase bg-white hover:bg-yellow-600 py-0 text-lg px-0 rounded-none text-black h-auto max-w-full min-w-4/5'}>
+                    <div className={'bg-yellow-500 px-12 py-4 flex-1'}>
                         complete your order
-                    </AsyncButton>
+                    </div>
+                </AsyncButton>
             </div>
         </form>
     </div>;
