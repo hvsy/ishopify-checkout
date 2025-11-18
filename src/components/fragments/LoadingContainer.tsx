23579507ccb533d6a8e2fb4bd66f5b517175e8c1
hvsy <sumenghua@live.com>
2025-11-18 19:24:02 +0800

    1.优化国家地区的接口 2.优化快递方式未选择的文本

--- src/components/fragments/LoadingContainer.tsx
+++ src/components/fragments/LoadingContainer.tsx
@@ -1,25 +1,27 @@
-import {FC, ReactNode} from "react";
-import {isString as  _isString} from "lodash-es";
+import {CSSProperties, FC, ReactNode} from "react";
+import {isString as  _isString,isFunction as _isFunction} from "lodash-es";
 import {cn} from "@lib/cn.ts";
 
 export type LoadingContainerProps = {
     loading : boolean;
-    children : ReactNode;
+    children : ReactNode|((loading : boolean)=>ReactNode);
     className ?: string;
     loadingClassName ?: string;
+    loadingContainerClassName ?: string;
+    style ?: CSSProperties;
 };
 
 export const LoadingContainer: FC<LoadingContainerProps> = (props) => {
-    const {loading,children,className = '',loadingClassName} = props;
-    const content=  _isString(children) ? <span>{children}</span> : children;
+    const {loading,style,children,loadingContainerClassName = '',className = '',loadingClassName} = props;
+    const content=  _isString(children) ? <span>{children}</span> : (_isFunction(children) ?children(loading) :children);
     const final = cn("relative select-none",{
         "max-w-104" : !(className?.includes('max-w-')),
         // "lab" : true,
         "*:invisible [&>*:last-child]:visible":loading,
     },className);
-    return <div className={final}>
+    return <div className={final} style={style}>
             {content}
-        {loading && <div className={`flex flex-row ustify-center items-center  visible absolute inset-0`} >
+        {loading && <div className={cn(`flex flex-row justify-center items-center  visible absolute inset-0`,loadingContainerClassName)} >
             <div className={`animate-pulse bg-slate-300 ${loadingClassName}`} />
         </div>}
     </div>
--- src/container/PaymentContext.tsx
+++ src/container/PaymentContext.tsx
@@ -3,7 +3,6 @@ import useSWR from "swr";
 import {Pixels} from "../shopify/fragments/Pixels.tsx";
 import {isString, get as _get} from "lodash-es";
 
-import {useSummary} from "../shopify/checkouts/hooks/useSummary.tsx";
 
 export const PaymentContext = createContext<{
     method: DB.PaymentMethod | null;
@@ -24,26 +23,32 @@ export function usePaymentMethod() {
 }
 
 export function useAllZones() {
-    const ctx = usePaymentContext();
-    return {zones: ctx?.zones || [], loading: ctx?.loading};
+    // const ctx = usePaymentContext();
+    const {data,isLoading} = useSWR("/a/s/api/zones/all");
+    return {zones: data|| [], loading: isLoading};
 }
 
 export function useShippingZones() {
-    const {json} = useSummary();
-    const {zones: all, loading} = useAllZones();
-    const ships2countries = _get(json, 'shop.shipsToCountries', []);
-    const marketCountries = _get(json, 'localization.availableCountries', []).map((country: any) => {
-        return country?.isoCode;
-    }).filter(Boolean);
+    const {data,isLoading} = useSWR('/a/s/api/zones');
     return {
-        zones: (ships2countries || []).map((code: string) => {
-            if (!marketCountries?.includes(code)) return null;
-            return all.find((region: any) => {
-                return region.code === code;
-            })
-        }).filter(Boolean),
-        loading,
+        zones : data || [],
+        loading:isLoading,
     }
+    // const {json} = useSummary();
+    // const {zones: all, loading} = useAllZones();
+    // const ships2countries = _get(json, 'shop.shipsToCountries', []);
+    // const marketCountries = _get(json, 'localization.availableCountries', []).map((country: any) => {
+    //     return country?.isoCode;
+    // }).filter(Boolean);
+    // return {
+    //     zones: (ships2countries || []).map((code: string) => {
+    //         if (!marketCountries?.includes(code)) return null;
+    //         return all.find((region: any) => {
+    //             return region.code === code;
+    //         })
+    //     }).filter(Boolean),
+    //     loading,
+    // }
 }
 
 function preload(config: any) {
