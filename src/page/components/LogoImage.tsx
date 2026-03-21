1c4a3e6adf68c0c74b28891ac4f01233d723bfcb
hvsy <sumenghua@live.com>
2026-03-21 13:32:42 +0800

    优化图片加载

--- src/page/components/LogoImage.tsx
+++ src/page/components/LogoImage.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {CSSProperties, FC} from "react";
 import {useSrcSet} from "@hooks/useSrcSet.ts";
 
 export type LogoImageProps = {
@@ -7,12 +7,14 @@ export type LogoImageProps = {
     height : number|string,
     alt ?: string;
     className ?: string;
+    style ?: CSSProperties,
 };
 
 export const LogoImage: FC<LogoImageProps> = (props) => {
-    const {url,width,height,alt,className} = props;
+    const {url,width,height,alt,style,className} = props;
     const srcSet = useSrcSet(url);
     return <img className={className}
+                style={style}
                 width={width}
                 height={height} src={url} srcSet={srcSet} alt={alt}/>;
 };
