64a0c8ac31cba671381ba8640de2cc79c2ead98f
hvsy <sumenghua@live.com>
2026-03-22 11:35:36 +0800

    优化界面的加载和渲染

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -1,5 +1,7 @@
-import {getArrayFromMeta} from "./metaHelper.ts";
+import {getArrayFromMeta, getJsonFromMeta} from "./metaHelper.ts";
 
 export const Features = getArrayFromMeta('features') || [];
 
 export const isCN = (new Date()).getTimezoneOffset() === -480;
+
+export const Setup = getJsonFromMeta('setup',null);
